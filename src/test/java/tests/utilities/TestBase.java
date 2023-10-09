8e266ba581c985b797f8be94592300ef3721c424
AhmetCan <a.canozyurek@gmail.com>
2023-10-09 11:56:18 +0300

    TestBaseStudies

---
+++ src/test/java/tests/utilities/TestBase.java
@@ -0,0 +1,25 @@
+package tests.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TestBase {
+   public WebDriver driver;
+    @Before
+    public void setUp(){
+        driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @After
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+       // driver.quit();
+    }
+}
